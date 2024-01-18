5b8dad30c83f91dcc2907ff0f846b551cd8a2868
@SoumyajitGhosh7 <ghoshsoumya501@gmail.com>
2024-01-18 20:55:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect,useRef } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -8,8 +8,11 @@ function App() {
   const [numAllowed, setnumAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
   const [Password, setPassword] = useState("")
-
+  const passwordRef=useRef(null)
+  const isClickref=useRef()
+  //const [isClicked,setisClicked]=useState(false)
   const passwordGenerator = useCallback(() => {
+    
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
@@ -20,13 +23,19 @@ function App() {
       pass += str[char]
     }
     setPassword(pass)
+    isClickref.current.style.backgroundColor="blue";
   }, [length, numAllowed, charAllowed, setPassword])
-
-
+ 
+  const copyClipboard=useCallback(()=>{
+    isClickref.current.style.backgroundColor="skyblue"
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,30)//when needed
+    window.navigator.clipboard.writeText(Password)
+  },[Password,length])
   useEffect(()=>{passwordGenerator()},[length, numAllowed, charAllowed, setPassword,passwordGenerator])
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-400 bg-gray-700'>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-80 text-orange-400 bg-gray-700'>
         <h1 className='text-white text-center '>Password generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
@@ -35,9 +44,12 @@ function App() {
             className='outline-none w-full py-1 px-3 '
             placeholder='password'
             readOnly
+            ref={passwordRef}
           />
           <button
-            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+            onClick={copyClipboard}
+            ref={isClickref}>Copy
           </button>
         </div>
         <div className='flex text-sm gap-x-2'>
@@ -53,7 +65,7 @@ function App() {
         <div className='flex items-center gap-x-1'>
           <input type="checkbox"
             defaultChecked={numAllowed}
-            onChange={() => setnumAllowed((prev) => !prev)}
+            onChange={() => setnumAllowed(!numAllowed)}
           />
           <label>Numbers</label>
         </div>
